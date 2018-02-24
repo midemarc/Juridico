4d7c130c05b22aaf3e73b32b7fd6680f73b2fdad
Marc Queudot <marc.queudot@viacesi.fr>
2018-02-24 18:41:29 -0500

    Add model access to admin interface

--- juridico_site/juridico/admin.py
+++ juridico_site/juridico/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Question)
+admin.site.register(Client)
+admin.site.register(Reponse)
+admin.site.register(Requete)
