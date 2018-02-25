66f7542ac51997bb6c01366fe0cc52f5174816c7
Marc Queudot <marc.queudot@viacesi.fr>
2018-02-24 19:44:13 -0500

    Begin dynamic question form
    juridico/question1 gets the content of Question object with qid=1

--- juridico_site/juridico/views.py
+++ juridico_site/juridico/views.py
@@ -9,17 +9,25 @@ from .forms import QuestionForm
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
 def questions(request):
     questions = serializers.serialize('json', Question.objects.all())
     return HttpResponse(questions)
 
-def question(request):
+
+def question(request, question_id):
+    question = Question.objects.filter(qid=question_id)[0]
     if request.method == 'POST':
         form = QuestionForm(request.POST)
-
         if form.is_valid():
             return HttpResponse('coucou')
     else:
-
         form = QuestionForm()
-    return render(request, 'question.html', {'form': form})
+    return render(
+        request,
+        'question.html',
+        {
+            'question_label': question.question,
+            'form': form
+        }
+      )
