66e4e474c4bd80a4b6b200838c92c8e21ccddb88
mok <mok@colomar>
2018-02-25 01:03:08 -0500

    Changements dans les models, ajout des tags,

--- juridico_site/juridico/models.py
+++ juridico_site/juridico/models.py
@@ -2,15 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+item_html = """<div class="item{extra_class}">
+<h4><a href="{url}">{titre}</a></h4>
+<div class="description"></div>
+</div>"""
+
+
+class Tag(models.Model):
+    tid = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=256)
+
 class Client(models.Model):
     pseudo = models.CharField(max_length=128, unique=True)
-    cid = models.IntegerField(primary_key=True)
+    cid = models.AutoField(primary_key=True)
     date_creation = models.DateTimeField(auto_now_add=True)
     date_modif = models.DateTimeField(auto_now=True)
     courriel = models.EmailField()
+    tags = models.ManyToManyField("Tag")
 
 class Question(models.Model):
-    qid = models.IntegerField(primary_key=True)
+    qid = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=128, unique=True)
     question = models.TextField()
     reponse_type = models.CharField(
@@ -26,7 +37,7 @@ class Question(models.Model):
     methode = models.CharField(max_length=32, default="")
 
 class Variable(models.Model):
-    vid = models.IntegerField(primary_key=True)
+    vid = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=32)
     valeur = models.CharField(max_length=1024)
     requete = models.ForeignKey("Requete", on_delete=models.CASCADE)
@@ -41,30 +52,8 @@ class Variable(models.Model):
         max_length=1
     )
 
-class Ressource(models.Model):
-    resid = models.IntegerField(primary_key=True)
-    requete = models.ForeignKey("Requete", on_delete=models.CASCADE)
-    description = models.TextField()
-
-class Organisation(Ressource):
-    nom = models.CharField(max_length=256)
-    url = models.CharField(max_length=1024)
-
-class Documentation(Ressource):
-    nom = models.CharField(max_length=256)
-    url = models.CharField(max_length=1024)
-
-class Camarade(Ressource):
-    client = models.ForeignKey("Client", on_delete=models.CASCADE)
-
-class Direction(models.Model):
-    dirid = models.IntegerField(primary_key=True)
-    quand = models.CharField(max_length=256)
-    contenu = models.TextField()
-
-
 class Reponse(models.Model):
-    repid = models.IntegerField(primary_key=True)
+    repid = models.AutoField(primary_key=True)
     question = models.ForeignKey("Question", on_delete=models.CASCADE)
     client = models.ForeignKey("Client", on_delete=models.CASCADE)
     requete = models.ForeignKey("Requete", on_delete=models.CASCADE)
@@ -74,8 +63,56 @@ class Reponse(models.Model):
     date_modif = models.DateTimeField(auto_now=True)
 
 class Requete(models.Model):
-    reqid = models.IntegerField(primary_key=True)
+    reqid = models.AutoField(primary_key=True)
     date_creation = models.DateTimeField(auto_now_add=True)
     date_modif = models.DateTimeField(auto_now=True)
     description_cas = models.TextField()
     client = models.ForeignKey("Client", on_delete=models.CASCADE)
+
+# Types de ressources
+
+class Ressource(models.Model):
+    resid = models.AutoField(primary_key=True, default=0, unique=True)
+    description = models.TextField(default="")
+    tags = models.ManyToManyField("Tag")
+
+    class Meta:
+        abstract = True
+
+class Organisation(Ressource):
+    nom = models.CharField(max_length=256)
+    url = models.CharField(max_length=1024)
+
+    def to_resultats(self):
+        return item_html.format(
+            url=self.url,
+            titre=self.nom,
+            description=self.description,
+            extra_class=""
+        )
+
+class Documentation(Ressource):
+    nom = models.CharField(max_length=256)
+    url = models.CharField(max_length=1024)
+
+    def to_resultats(self):
+        return item_html.format(
+            url=self.url,
+            titre=self.nom,
+            description=self.description,
+            extra_class=""
+        )
+
+class Camarade(Ressource):
+    client = models.ForeignKey("Client", on_delete=models.CASCADE)
+
+    def to_resultats(self):
+        return item_html.format(
+            url="",
+            titre=self.nom,
+            description=self.description,
+            extra_class=""
+        )
+
+class Direction(Ressource):
+    quand = models.CharField(max_length=256)
