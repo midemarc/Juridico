3d5f380f7b43495904b58375737f9bf22f229894
Marc Queudot <queudot.marc@courrier.uqam.ca>
2018-03-06 16:07:05 -0500

    Bugfix (url for accessing a specific question)
    The parentheses were from when I used a regex ; it is not valid anymore

--- juridico_site/juridico/urls.py
+++ juridico_site/juridico/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('resultats/<int:reqid>/', views.resultats),
     path('requete/client<int:cid>', views.requete),
     # path('<int:question_id>/vote/', views.question, name='question'),
-    path('api/questions(<int:question_id>)', views.api_question),
+    path('api/questions<int:question_id>', views.api_question),
     path('api/questions', views.api_questions),
     path('api/reponses', views.api_reponses),
 ]
