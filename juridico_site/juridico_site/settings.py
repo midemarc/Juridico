376ca81824682fe93db0c1f24982307c80595926
Marc Queudot <queudot.marc@courrier.uqam.ca>
2018-04-17 16:31:24 -0400

    Moved database (to accomodate docker volume)

--- juridico_site/juridico_site/settings.py
+++ juridico_site/juridico_site/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'juridico_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
